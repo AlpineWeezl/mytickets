9fb2d94be8465712a3ff925d5b097652b65bed8e
Matthias Wieser <matthias.wieser.83@live.de>
2022-07-10 17:56:01 +0200

    prepare PWA

--- src/components/PassCard.js
+++ src/components/PassCard.js
@@ -1,16 +1,33 @@
-import { Card, CardContent, CardHeader, Container } from '@mui/material'
+import { Card, CardContent, CardHeader, Container, Divider } from '@mui/material'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const PassCard = ({ pass }) => {
-    return (
-        <Container sx={{ my: 3 }}>
-            <Card>
-                <CardHeader title={pass.title} />
-                <CardContent>
+    const navigate = useNavigate();
 
+    const clickHandler = () => {
+        navigate(`/passes/${pass._id}`);
+    }
+
+    return (
+            <div onClick={clickHandler} className={'bg-white my-auto'}>
+                <CardHeader title={pass.title} />
+                <Divider />
+                <CardContent>
+                    <h3>{pass.description ? pass.description : 'Keine Beschreibung vorhanden.'}</h3>
+                    <Divider />
+                    <Container sx={{ display: 'flex' }}>
+                        <Container sx={{ display: 'flex', flexDirection: 'column', textAlign: 'left' }} >
+                            <h4 className='my-0'>Kaufpreis:</h4>
+                            <p>{pass.price} €</p>
+                            <h4>Benutzt:</h4>
+                            <p>{pass.price} €</p>
+                        </Container>
+                        <Container sx={{ display: 'flex', flexDirection: 'column', textAlign: 'left' }}>
+                        </Container>
+                    </Container>
                 </CardContent>
-            </Card>
-        </Container>
+            </div>
     )
 }
 
