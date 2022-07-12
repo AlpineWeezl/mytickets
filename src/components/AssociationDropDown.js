ab96ac8eddf4c8dd4d94caa0dbf8117ca8bca2a7
Matthias Wieser <matthias.wieser.83@live.de>
2022-07-12 11:47:30 +0200

    add association dropdown to pass editor

--- src/components/AssociationDropDown.js
+++ src/components/AssociationDropDown.js
@@ -1,43 +1,85 @@
+import { ArrowDropDown, ArrowDropUp } from "@mui/icons-material";
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { authContext } from "../context/authContext";
 
-const AssociationDropDown = ({ pass, setSelectedAssociation }) => {
-    const { dateFormat, verified, token } = useContext(authContext);
+const AssociationDropDown = ({ pass, selectedAssociation, setSelectedAssociation }) => {
+    const { token } = useContext(authContext);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [associations, setAssociations] = useState(null);
-    const { usageId } = useParams();
+    const [isOpen, setIsOpen] = useState(false);
     const navigate = useNavigate();
     const apiUrl = process.env.REACT_APP_API_URL;
 
-
     useEffect(() => {
-        !verified && navigate('/login');
-        if (pass) {
-            axios
-                .get(`${apiUrl}/associations/`, { headers: { authorization: token } })
-                .then(res => {
-                    console.log(res.data);
-                    setAssociations(res.data.associations);
-                    setLoading(false);
-                })
-                .catch(err => {
-                    toast.error('Die Nutzung konnte nicht geladen werden!');
-                    console.log(err);
-                    setError(err);
-                    setLoading(false);
-                });
-        } else {
-            setLoading(false);
-        }
-    }, [apiUrl, navigate, pass, token, usageId, verified]);
+        axios
+            .get(`${apiUrl}/associations`, { headers: { authorization: token } })
+            .then(res => {
+                setAssociations(res.data.associations);
+                const setInitial = async (associationId) => {
+                    const association = await res.data.associations.find(ass => ass._id === associationId);
+                    await setSelectedAssociation(association);
+                }
+                pass && setInitial(pass.associationId);
+                setLoading(false);
+            })
+            .catch(err => {
+                toast.error('Verbünde konnten nicht geladen werden!');
+                console.log(err);
+                setError(err);
+                setLoading(false);
+            });
+    }, [apiUrl, navigate, pass, setSelectedAssociation, token]);
+
+    const toggleOpenHandler = () => {
+        setIsOpen(!isOpen);
+    }
+
+    const selectAssociationHandler = async (e) => {
+        const { id } = e.target;
+        await axios
+            .get(`${apiUrl}/associations/${id}`, { headers: { authorization: token } })
+            .then(res => {
+                setSelectedAssociation(res.data.association);
+            })
+            .catch(err => {
+                toast.error('Die Verbünde konnte nicht gesetzt werden!');
+                console.log(err);
+                setLoading(false);
+            });
+        setIsOpen(false);
+    }
+
+    if (error) { return <h3>Error...</h3> }
+    if (loading) { return <h3>Loading...</h3> }
 
     return (
-        <div>AssociationDropDown</div>
+        <div className="flex flex-col pt-5">
+            <button onClick={toggleOpenHandler} className="flex p-4 border rounded justify-center items-center shadow-md">
+                {selectedAssociation ? selectedAssociation.title : 'Bitte wähle einen Verbund aus'}
+                {!isOpen ? <ArrowDropDown /> : <ArrowDropUp />}
+            </button>
+            <div>
+                <div className="absolute w-[85%] border shadow-md rounded-md bg-white divide-y max-h-[70vh] overflow-y-scroll" hidden={!isOpen}>
+                    {
+                        associations && associations.map((association, i) => {
+                            return <button
+                                id={association._id}
+                                key={association._id}
+                                onClick={selectAssociationHandler}
+                                className={`w-full p-3 ${(i % 2 === 0 && 'bg-slate-100')}`}
+                            >
+                                {association.title}
+                            </button>
+                        })
+                    }
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default AssociationDropDown
+export default AssociationDropDown;
