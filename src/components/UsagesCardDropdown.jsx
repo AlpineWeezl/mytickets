194b2b8e386b8a3ff802b7784b774c66ba48a212
Matthias Wieser <matthias.wieser.83@live.de>
2022-07-15 10:59:03 +0200

    add a icon to usages card dropdown

--- src/components/UsagesCardDropdown.jsx
+++ src/components/UsagesCardDropdown.jsx
@@ -1,3 +1,4 @@
+import { ArrowDropDownOutlined, ArrowDropUpOutlined } from "@mui/icons-material";
 import { useState } from "react"
 import UsagesTable from "./UsagesTable";
 
@@ -6,7 +7,14 @@ const UsagesPassDropdown = ({ pass }) => {
 
     return (
         <>
-            <button onClick={() => setIsOpen(!isOpen)} className={'py-2 border-b-2 text-2xl font-bold'}>{pass.title}</button>
+            <button
+                onClick={() => setIsOpen(!isOpen)}
+                className={'flex justify-center items-center gap-3 py-2 border-b-2 text-2xl font-bold'}>
+                {pass.title}
+                <div className="flex justify-center items-center w-8 h-8 rounded-full border-2 border-black">
+                    {!isOpen ? <ArrowDropDownOutlined fontSize="large" /> : <ArrowDropUpOutlined fontSize="large" />}
+                </div>
+            </button>
             {
                 (isOpen) && (
                     <UsagesTable pass={pass} />
