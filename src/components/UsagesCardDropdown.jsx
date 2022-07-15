5a4f07745cf5569fa4919ab04ebcef1caf68fdfd
Matthias Wieser <matthias.wieser.83@live.de>
2022-07-15 17:41:34 +0200

    implement pie chart to visualize the amount of usages

--- src/components/UsagesCardDropdown.jsx
+++ src/components/UsagesCardDropdown.jsx
@@ -1,27 +1,37 @@
-import { ArrowDropDownOutlined, ArrowDropUpOutlined } from "@mui/icons-material";
-import { useState } from "react"
+import {
+	ArrowDropDownOutlined,
+	ArrowDropUpOutlined,
+} from "@mui/icons-material";
+import { useState } from "react";
+import UsagesChart from "./UsagesChart";
 import UsagesTable from "./UsagesTable";
 
 const UsagesPassDropdown = ({ pass }) => {
-    const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <>
-            <button
-                onClick={() => setIsOpen(!isOpen)}
-                className={'flex justify-center items-center gap-3 py-2 border-b-2 text-2xl font-bold'}>
-                {pass.title}
-                <div className="flex justify-center items-center w-8 h-8 rounded-full border-2 border-black">
-                    {!isOpen ? <ArrowDropDownOutlined fontSize="large" /> : <ArrowDropUpOutlined fontSize="large" />}
-                </div>
-            </button>
-            {
-                (isOpen) && (
-                    <UsagesTable pass={pass} />
-                )
-            }
-        </>
-    )
-}
+	return (
+		<>
+			<button
+				onClick={() => setIsOpen(!isOpen)}
+				className="flex justify-center items-center gap-3 py-3 border-b-2 text-2xl font-bold bg-white"
+			>
+				{pass.title}
+				<div className="flex justify-center items-center w-8 h-8 rounded-full border-2 border-black">
+					{!isOpen ? (
+						<ArrowDropDownOutlined fontSize="large" />
+					) : (
+						<ArrowDropUpOutlined fontSize="large" />
+					)}
+				</div>
+			</button>
+			{isOpen && (
+				<>
+					<UsagesChart pass={pass} />
+					<UsagesTable pass={pass} />
+				</>
+			)}
+		</>
+	);
+};
 
-export default UsagesPassDropdown
+export default UsagesPassDropdown;
---
+++ src/components/UsagesChart.jsx
@@ -0,0 +1,146 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useContext } from "react";
+import "chart.js/auto";
+import { Chart } from "react-chartjs-2";
+import { toast } from "react-toastify";
+import { authContext } from "../context/authContext";
+import LoaderPage from "./LoaderPage";
+
+const UsagesChart = ({ pass }) => {
+	const { token } = useContext(authContext);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+	const [usages, setUsages] = useState(null);
+	const [companies, setCompanies] = useState(null);
+	const [numBest, setNumBest] = useState(3);
+	const apiUrl = process.env.REACT_APP_API_URL;
+
+	useEffect(() => {
+		axios
+			.get(`${apiUrl}/companies`, {
+				headers: { authorization: token },
+			})
+			.then((res) => {
+				setCompanies(res.data.companies);
+			})
+			.catch((err) => {
+				toast.error("Das Diagramm konnte nicht erstellt werden!");
+				console.log(err);
+				setError(err);
+				setLoading(false);
+			});
+		axios
+			.get(`${apiUrl}/usages/pass/${pass._id}`, {
+				headers: { authorization: token },
+			})
+			.then((res) => {
+				setUsages(res.data.usages);
+				setLoading(false);
+			})
+			.catch((err) => {
+				toast.error("Das Diagramm konnte nicht erstellt werden!");
+				console.log(err);
+				setError(err);
+				setLoading(false);
+			});
+	}, [apiUrl, pass, token]);
+
+	if (!companies && loading) {
+		return <LoaderPage />;
+	}
+
+	if (error) {
+		return <h2>Error...</h2>;
+	}
+
+	let bestCompanies;
+	if (companies && !loading) {
+		const companyValue = companies.map((company) => {
+			const companyUsages = usages.filter((usage) => {
+				return usage.companyId === company._id;
+			});
+			const valuePerCompanyAndUsage = companyUsages.map((cmpnyUsg) => {
+				return cmpnyUsg.price;
+			});
+
+			const reducedValue = valuePerCompanyAndUsage.reduce(
+				(curr, prev) => {
+					return curr + prev;
+				},
+				0
+			);
+			return { label: company.title, value: reducedValue };
+		});
+
+		const sortedCompanies = companyValue.sort((a, b) => {
+			return b.value - a.value;
+		});
+		console.log(sortedCompanies);
+
+		const chartLabels = sortedCompanies.map((cmpny) => {
+			return cmpny.label;
+		});
+
+		const chartData = sortedCompanies.map((cmpny) => {
+			return cmpny.value;
+		});
+
+		bestCompanies = {
+			labels: chartLabels.splice(0, numBest),
+			datasets: [
+				{
+					data: chartData.splice(0, numBest),
+					backgroundColor: [
+						"rgba(255, 99, 132, 0.7)",
+						"rgba(54, 162, 235, 0.7)",
+						"rgba(255, 206, 86, 0.7)",
+						"rgba(75, 192, 192, 0.7",
+						"rgba(153, 102, 255, 0.7)",
+						"rgba(255, 159, 64, 0.7)",
+						"rgba(163, 192, 86, 0.7)",
+						"rgba(255, 159, 64, 0.7)",
+						"rgba(255, 159, 64, 0.7)",
+						"rgba(255, 159, 64, 0.7)",
+					],
+					borderColor: [
+						"rgba(255, 99, 132, 1)",
+						"rgba(54, 162, 235, 1)",
+						"rgba(255, 206, 86, 1)",
+						"rgba(75, 192, 192, 1)",
+						"rgba(153, 102, 255, 1)",
+						"rgba(255, 159, 64, 1)",
+						"rgba(255, 159, 64, 1)",
+						"rgba(255, 159, 64, 1)",
+						"rgba(255, 159, 64, 1)",
+						"rgba(255, 159, 64, 1)",
+					],
+					borderWidth: 1,
+				},
+			],
+		};
+	}
+
+	return (
+		<div>
+			<>
+				{bestCompanies && (
+					<Chart type="pie" data={bestCompanies} height={"50%"} />
+				)}
+				<select
+					onChange={(e) => setNumBest(e.target.value)}
+					name="noBest"
+					id="noBest"
+					className="p-2 text-right m-3"
+				>
+					<option value="3">3</option>
+					<option value="5">5</option>
+					<option value="7">7</option>
+					<option value="10">10</option>
+				</select>
+			</>
+		</div>
+	);
+};
+
+export default UsagesChart;
